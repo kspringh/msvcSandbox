7b099f4102d7c665f4896225d3ebf6dc6e598997
Kenneth Springhetti <kz1000@hotmail.com>
2020-09-21 14:47:08 -0400

    lets try again

---
+++ HelloWorldSeeSharp/HelloWorldSeeSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldSeeSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World See Sharp!");
+        }
+    }
+}
